95ee615c3359b41fcbbbe1e17004d6dc41229635
TigranBalayan10 <tigranbalayan10@gmail.com>
2024-03-29 02:39:44 -0700

    sections done READY

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function addToCart(productName, productDescription, productPrice) {
-  window.location.href = 'product.html?product=' + encodeURIComponent(productName) + '&description=' + encodeURIComponent(productDescription) + '&price=' + encodeURIComponent(productPrice);
+  window.location.href =
+    "product.html?product=" +
+    encodeURIComponent(productName) +
+    "&description=" +
+    encodeURIComponent(productDescription) +
+    "&price=" +
+    encodeURIComponent(productPrice);
+}
+
+function submitForm() {
+  var form = document.getElementById("contactForm");
+  var formData = new FormData(form);
+  var formValues = {};
+
+  // Convert form data to object
+  for (var pair of formData.entries()) {
+    formValues[pair[0]] = pair[1];
+  }
+
+  // Display form values in an alert
+  var message = "";
+  for (var key in formValues) {
+    message += key + ": " + (formValues[key] || "none") + "\n";
+  }
+  alert(message);
 }
